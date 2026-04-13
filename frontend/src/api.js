4ec8d6fab5538a85ab2896639e1be69a75267774
shubhammeena11 <shubhammeena982@gmail.com>
2026-04-13 23:37:22 +0530

    .env created in front end for render URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { store } from "./redux/store.js";
 import { logout } from "./redux/authSlice.js";
+const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: API,
 });
 
 export const logoutUser = () => {
